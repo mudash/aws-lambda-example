f688bf44b0fd0888f8aa5da7b9fb3f1e34b86a51
Mudassar Shafique <15931574+mudash@users.noreply.github.com>
2021-08-12 20:42:58 -0700

    go compile settings
    Signed-off-by: Mudassar Shafique <15931574+mudash@users.noreply.github.com>

--- build-func.sh
+++ build-func.sh
@@ -5,5 +5,5 @@ echo -e "\n Building Go lambda function\n"
 rm -rf ./bin
 
 cd go-func
-env GOOS=linux GOARCH=amd64 go build -o ../bin/get-name
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ../bin/get-name
 cd ..
